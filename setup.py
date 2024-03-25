5a3017a265f1511cacc5a7dcc6629cd1f9109285
Carlos Trejo <37058445+CarlosGTrejo@users.noreply.github.com>
2024-03-25 13:43:01 -0500

    changed url to see if it'll work now

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='David Kovar',
     author_email='dkovar@gmail.com',
     packages=['analyzemft'],
-    url='http://github.com/dkovar/analyzeMFT',
+    url='https://github.com/CarlosGTrejo/analyzeMFT',
     license='LICENSE.txt',
     description='Analyze the $MFT from a NTFS filesystem.',
     long_description=open('README.txt').read(),
